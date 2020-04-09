f6f430b046a184930c46383e814087e3fa0c66ea
Tim-Bullock <60457142+Tim-Bullock@users.noreply.github.com>
2020-04-09 15:56:42 -0400

    trying to get gihub pages to load

--- DA5/js/Boot.js
+++ DA5/js/Boot.js
@@ -5,10 +5,10 @@ class BootScene extends Phaser.Scene {
     
     preload() {
         // Load assets required for the Preloader Scene
-        this.load.image('menu_background', '../assets/menu_background.png');
-        this.load.image('logo', '../assets/logo.png');
-        this.load.image('button1', '../assets/button1.png');
-        this.load.image('button2', '../assets/button2.png');
+        this.load.image('menu_background', 'assets/menu_background.png');
+        this.load.image('logo', 'assets/logo.png');
+        this.load.image('button1', 'assets/button1.png');
+        this.load.image('button2', 'assets/button2.png');
     }
     
     create() {
